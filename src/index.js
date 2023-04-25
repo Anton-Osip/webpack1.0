7e9b38dab22b7b37928a706926cf49013e7f53f0
Anton-Osip <osipchika@gmail.com>
2023-04-25 15:37:55 +0300

    9. Создание React компонента и работа с css модулями

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { App } from './App'
 
 const domNode = document.getElementById('root')
 const root = createRoot(domNode)
 
-const app = <h1>This is NEW REACT APP!!!!</h1>
+const app = <App />
 
 root.render(app)
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
+require('@babel/polyfill')
 const path = require('path')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-	entry: { main: './src/index.js' },
+	entry: { main: ['@babel/polyfill', './src/index.js'] },
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: '[name].[contenthash].js',
