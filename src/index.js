aa986327eb9a853f51187472c3325c1abdac8558
Anton-Osip <osipchika@gmail.com>
2023-04-25 14:44:03 +0300

    6. Установка file loader и обработка jpgpngjpeg и других файлов

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import Person from './person'
 import './styles/styles.css'
 import './styles/style.scss'
+import web from './images/webpack.png'
 
 const Anton = new Person('Anton', 26, 'Minsk')
 
 console.log(Anton)
+
+document.querySelector('.img').src = web
